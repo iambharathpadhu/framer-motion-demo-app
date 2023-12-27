a69db9e7abe6ffd71e60c2648653d08c49fe0b0b
Barath <iambharathpadhu@gmail.com>
2023-12-27 18:20:16 +0530

    - chore: removed extra motion div on accordion

--- src/Components/Accordion.tsx
+++ src/Components/Accordion.tsx
@@ -1,31 +1,20 @@
 import { useState } from "react";
 import { SimpleAccordion } from "./SimpleAccordion";
 import { AnimatedAccordion } from "./AnimatedAccordion";
-import { AnimatePresence, motion } from "framer-motion";
 
 export function Accordion() {
   const [isAnimated, toggleAnimated] = useState(false);
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        initial={{ opacity: 0, y: -20 }}
-        animate="visible"
-        exit={{ opacity: 0, transition: { duration: 2.0, delay: 0.5 } }}
-        variants={{
-          hidden: { opacity: 0, y: -20 },
-          visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+    <div>
+      <button
+        onClick={() => {
+          toggleAnimated((prev) => !prev);
         }}
       >
-        <button
-          onClick={() => {
-            toggleAnimated((prev) => !prev);
-          }}
-        >
-          Animate my accordion!
-        </button>
-        {isAnimated ? <AnimatedAccordion /> : <SimpleAccordion />}
-      </motion.div>
-    </AnimatePresence>
+        Animate my accordion!
+      </button>
+      {isAnimated ? <AnimatedAccordion /> : <SimpleAccordion />}
+    </div>
   );
 }
